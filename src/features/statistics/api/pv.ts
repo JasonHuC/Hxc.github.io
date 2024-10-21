7e558151e30f8ade10b4a5a88803458380ff1bc0
jasonhu <h1905659226@gmail.com>
2024-10-21 18:12:46 +0800

    删除不必要的uv,pv函数

--- src/features/statistics/api/pv.ts
+++ src/features/statistics/api/pv.ts
@@ -1,11 +1,8 @@
 import { useRequest } from 'ahooks';
 
-import { getPV, recordPV } from '../actions';
+import { recordPV } from '../actions';
 
 export const useRecordPV = () => {
   return useRequest(recordPV, { manual: true });
 };
 
-export const useGetPV = () => {
-  return useRequest(() => getPV());
-};
