f0f9d894f507883acb560fa27ae06a523bbee788
jasonhu <h1905659226@gmail.com>
2024-10-22 23:38:41 +0800

    测试打包

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,2 @@
 export const DATABASE_URL = process.env.DATABASE_URL;
-export const REDIS_HOST = process.env.REDIS_HOST;
-
-export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_URL = process.env.REDIS_URL
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,15 +1,16 @@
 import { Redis,type Redis as RedisInstanceType } from "ioredis"
-import { NODE_ENV, REDIS_HOST, REDIS_PORT } from '@/config';
+import { NODE_ENV, REDIS_URL } from '@/config';
 
 import { REDIS_KYE_PREFIX } from '@/constants';
 
 const globalForRedis = global as unknown as { redis: RedisInstanceType };
 
+if (!REDIS_URL) {
+    throw new Error("REDIS_URL is not defined");
+}
 export const redis =
     globalForRedis.redis ||
-    new Redis({
-        host: REDIS_HOST || '127.0.0.1',
-        port: Number(REDIS_PORT) || 6379,
+    new Redis(REDIS_URL as string, {
         keyPrefix: REDIS_KYE_PREFIX,
     });
 
