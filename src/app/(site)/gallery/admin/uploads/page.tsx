10f17e37d384157e7524ea63f83f713893180588
jasonhu <h1905659226@gmail.com>
2024-10-23 12:15:57 +0800

    修改admin/upload下的maxduration为1-60之间的数，测试打包

--- src/app/(site)/gallery/admin/uploads/page.tsx
+++ src/app/(site)/gallery/admin/uploads/page.tsx
@@ -3,7 +3,7 @@ import SiteGrid from '@/features/gallery/components/SiteGrid';
 import { getUniqueTagsCached } from '@/features/gallery/photo/cache';
 import AdminUploadsClient from '@/app/admin/gallery/AdminUploadsClient';
 
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export default async function AdminUploadsPage() {
     const urls = await getStorageUploadUrlsNoStore();
